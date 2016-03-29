b9243d280ff99c8bb397fe7ed9f191b65be1194b
ycohn <cohn.yehuda@gmail.com>
2016-03-29 10:15:14 -0400

    create course, update associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :courses, through: :students_courses
   has_many :courses, :foreign_key => 'teacher_id'
-  has_many :courses, through: :students_courses, :foreign_key => 'student_id'
   has_many :logs
   has_many :books, through: :readers
+  has_many :students_courses, :foreign_key => 'student_id'
   has_secure_password
+
+def full_name
+  @full_name = "#{self.first_name} #{self.last_name}"
 end
+
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+
   get 'sessions/new', to: "sessions#new"
   delete 'sessions/destroy' => 'sessions#destroy'
 
 
+  
   resources :sessions, only:[:new, :create]
   resources :registrations, only: [:new,:create]
   resources :users
-  
+  resources :courses
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
