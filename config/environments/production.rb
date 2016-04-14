a8c6bd1a8614d39a5aaa5555ed0b41a1d1d3b2bb
ycohn <cohn.yehuda@gmail.com>
2016-04-14 18:02:24 -0400

    update email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.raise_delivery_errors = true
+  host = 'voraciousapp.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'smtp' }
+
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
