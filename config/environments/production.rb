7cef1a72c3e7658fcd00ec6c176d871eba9a6ac7
ycohn <cohn.yehuda@gmail.com>
2016-04-14 23:58:37 -0400

    adjust

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,17 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'voraciousapp.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address: "smtp.sendgrid.net",
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
     port: 587,
-    domain: "heroku.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"]
+    user_name: ENV["gmail_username"],
+    password: ENV["gmail_password"]
   }
 end
